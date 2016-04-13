37b77cf7469e53afe3b7c00b602d6424a7ae9488
Sam Siu <samsiu@Sams-MacBook-Pro.local>
2016-04-13 13:55:10 -0700

    SyncAdapter: Added Manual Sync to FAB Button

--- SyncAdapterLab/app/src/main/java/anthony/example/com/syncadapterlab/MainActivity.java
+++ SyncAdapterLab/app/src/main/java/anthony/example/com/syncadapterlab/MainActivity.java
@@ -1,5 +1,9 @@
 package anthony.example.com.syncadapterlab;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,15 +12,37 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    // Constants
+    // Content provider authority
+    public static final String AUTHORITY = "siu.example.com.syncadapter.StubProvider";
+    // Account type
+    public static final String ACCOUNT_TYPE = "example.com";
+    // Account
+    public static final String ACCOUNT = "default_account";
+
+    Account mAccount;
+
+    Button syncButton;
+
+    // Global variables
+    // A content resolver for accessing the provider
+    ContentResolver mResolver;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mAccount = createSyncAccount(this);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +50,18 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Bundle settingsBundle = new Bundle();
+                settingsBundle.putBoolean(
+                        ContentResolver.SYNC_EXTRAS_MANUAL, true);
+                settingsBundle.putBoolean(
+                        ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
+                /*
+                 * Request the sync for the default account, authority, and
+                 * manual sync settings
+                 */
+                ContentResolver.requestSync(mAccount, AUTHORITY, settingsBundle);
+
+
             }
         });
     }
@@ -49,4 +87,41 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    /**d
+     * Create a new dummy account for the sync adapter
+     *
+     * @param context The application context
+     */
+    public static Account createSyncAccount(Context context) {
+        // Create the account type and default account
+        Account newAccount = new Account(
+                ACCOUNT, ACCOUNT_TYPE);
+        // Get an instance of the Android account manager
+        AccountManager accountManager =
+                (AccountManager) context.getSystemService(
+                        ACCOUNT_SERVICE);
+        /*
+         * Add the account and account type, no password or user data
+         * If successful, return the Account object, otherwise report an error.
+         */
+        if (accountManager.addAccountExplicitly(newAccount, null, null)) {
+          /*
+           * If you don't set android:syncable="true" in
+           * in your <provider> element in the manifest,
+           * then call context.setIsSyncable(account, AUTHORITY, 1)
+           * here.
+           */
+        } else {
+            /*
+             * The account exists or some other error occurred. Log this, report it,
+             * or handle it internally.
+             */
+        }
+        return newAccount;
+    }
+
+
+
 }
